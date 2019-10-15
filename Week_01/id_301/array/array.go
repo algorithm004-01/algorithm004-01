e9db2af6ccdd21a845dccb219c9c1256f612e0f2
yangfeng <yangfeng@soyoung.com>
2019-10-15 22:36:31 +0800

    301-Week 01

--- Week_01/id_301/array/array.go
+++ Week_01/id_301/array/array.go
@@ -2,6 +2,9 @@ package array
 
 import "fmt"
 
+/**
+移除0
+*/
 func removeDuplicates(nums []int) int {
 	length := len(nums)
 	index := 0
@@ -14,7 +17,7 @@ func removeDuplicates(nums []int) int {
 	return index + 1
 }
 
-/**
+/** 向右移动数组解法1
 解法思想 ： 当一个数组长度为n，移动后k次，当k<n的时候很好理解，当k>=n的时候，其实本质上是移动了k对n求余次数
 */
 func RotateOne(nums []int, k int) {
@@ -31,7 +34,7 @@ func RotateOne(nums []int, k int) {
 	}
 }
 
-/**
+/** 向右移动数组解法2
 解法思想 ： 移动次数简化同上面分析。然后我们从最数组的第一位开始，挨个一次将元素移动到目标位置，移动n次，那么所有元素都将到达最后的位置.
 		  如果内层刚好移动到
 */
@@ -60,6 +63,9 @@ func RotateTwo(nums []int, k int) {
 	}
 }
 
+/**
+向右移动数组解法三 三次反转法
+*/
 func RotateThree(nums []int, k int) {
 	length := len(nums)
 	if length <= 1 {
@@ -71,6 +77,9 @@ func RotateThree(nums []int, k int) {
 	reverseArray(nums, k, length-1)
 }
 
+/**
+反转数组
+*/
 func reverseArray(nums []int, start, end int) {
 	length := len(nums)
 	if length <= 1 {
@@ -90,5 +99,38 @@ func reverseArray(nums []int, start, end int) {
 	for i := start; i < j; i++ {
 		nums[i], nums[j] = nums[j], nums[i]
 		j--
+	}
+}
+
+/**
+合并两个有序数组 双指针从尾部开始处理数据
+*/
+func Merge(nums1 []int, m int, nums2 []int, n int) {
+	p := m + n - 1
+	p1 := m - 1
+	p2 := n - 1
+	for {
+		if p1 < 0 {
+			for ; p2 >= 0; p2-- {
+				nums1[p] = nums2[p2]
+				p--
+			}
+			break
+		}
+		if p2 < 0 {
+			for ; p1 >= 0; p1-- {
+				nums1[p] = nums1[p1]
+				p--
+			}
+			break
+		}
+		if nums1[p1] > nums2[p2] {
+			nums1[p] = nums1[p1]
+			p1--
+		} else {
+			nums1[p] = nums2[p2]
+			p2--
+		}
+		p--
 	}
 }
--- Week_01/id_301/main.go
+++ Week_01/id_301/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"algo04-01/Week_01/id_301/linklist"
+	"algo04-01/Week_01/id_301/array"
 	"fmt"
 )
 
@@ -9,20 +9,24 @@ func main() {
 	//nums := []int{1,2,3,4,5,6}
 	//array.RotateThree(nums,2)
 	//fmt.Println(nums)
-	var l1 *linklist.ListNode
-	var l2 *linklist.ListNode
-	l1 = new(linklist.ListNode)
-	l1.Val = 1
-	l1.Next = new(linklist.ListNode)
-	l1.Next.Val = 4
-	l1.Next.Next = new(linklist.ListNode)
-	l1.Next.Next.Val = 6
-	l2 = new(linklist.ListNode)
-	l2.Val = 2
-	l2.Next = new(linklist.ListNode)
-	l2.Next.Val = 3
-	l2.Next.Next = new(linklist.ListNode)
-	l2.Next.Next.Val = 7
+	//var l1 *linklist.ListNode
+	//var l2 *linklist.ListNode
+	//l1 = new(linklist.ListNode)
+	//l1.Val = 1
+	//l1.Next = new(linklist.ListNode)
+	//l1.Next.Val = 4
+	//l1.Next.Next = new(linklist.ListNode)
+	//l1.Next.Next.Val = 6
+	//l2 = new(linklist.ListNode)
+	//l2.Val = 2
+	//l2.Next = new(linklist.ListNode)
+	//l2.Next.Val = 3
+	//l2.Next.Next = new(linklist.ListNode)
+	//l2.Next.Next.Val = 7
 	//fmt.Println(l1.Val)
-	fmt.Println(linklist.MergeTwoLists(l1, l2).Next.Next.Next.Next.Val)
+	//fmt.Println(linklist.MergeTwoLists(l1, l2).Next.Next.Next.Next.Val)
+	a1 := []int{1, 3, 9, 0, 0, 0, 0, 0}
+	a2 := []int{2, 5, 6}
+	array.Merge(a1, 3, a2, 3)
+	fmt.Println(a1)
 }
