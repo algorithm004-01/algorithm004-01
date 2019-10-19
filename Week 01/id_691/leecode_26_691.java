c5ac96a726a3e592d23c4ee5c7fbbfed1e75dde1
Rosy Huang <1042915541@qq.com>
2019-10-19 14:45:59 +0800

    Create leecode_26_691.java

---
+++ Week 01/id_691/leecode_26_691.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        //双指针
+        if (nums.length == 0) return  0;
+        int i = 0;
+        for(int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+    
+    //国际站5步
+    public int removeDuplicates(int[] nums){
+        int i = 0;
+        for ( int n : nums ){
+            if (i == 0 || n > nums[i-1]){
+                nums[i++] = n;
+            }
+            return i;
+        }
+
+    }
+
+}
