6ba467850e9d9dd63d835d00c33468b3bc1d58c4
神魔佛 <SFLXZT@163.com>
2019-10-20 18:00:50 +0800

    Update 189.旋转数组.py

--- Week 01/id_381/189.旋转数组.py
+++ Week 01/id_381/189.旋转数组.py
@@ -3,14 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
+        k = k % len(nums)   # 防止k太大
         start = 0
+        # start + k 的值超出长度后循环next
         next = (start + k) % len(nums)
         tmp = nums[start]
         for i in range(len(nums)):
             nums[next], tmp = tmp, nums[next]
+            # 相等会陷入循环，其他数据无法挪动
             if next == start:
                 if i == len(nums)-1:
+                    # 结束
                     break
                 start += 1
                 next = (start + k) % len(nums)
