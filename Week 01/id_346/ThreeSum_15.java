251dc5cf61b4f6fb8290c42af31e197ed3b1e7a8
tukaiqiang <tukaiqiang@ppdai.com>
2019-10-20 17:34:39 +0800

    346-Week 01

--- Week 01/id_346/ThreeSum_15.java
+++ Week 01/id_346/ThreeSum_15.java
@@ -1,4 +1,4 @@
-package com;
+package com.yiche.mapi;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,23 +6,18 @@ import java.util.List;
 
 /**
  * @auther: TKQ
- * @Title: ThreeSum
+ * @Title: ThreeSum_15
  * @Copyright: Copyright (c) 2019
  * @Description:
  * @Company:
- * @Created: 2019-10-17 23:58
+ * @Created: 2019-10-20 17:21
  */
-public class ThreeSum {
-    public static void main(String[] args) {
-        List<List<Integer>> lists = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
-        System.out.println(lists);
-    }
-
-    public static List<List<Integer>> threeSum(int[] nums) {
+public class ThreeSum_15 {
+    public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         for (int i = 0; i < nums.length-2; i++) {
-            if(nums[i] > 0){break;}
+            if(nums[i] > 0){ break;}
             if(i>0&&nums[i]==nums[i-1]){continue;}
             for(int l = i+1,r = nums.length-1;l<r;){
                 int sum = nums[i] + nums[l] + nums[r];
@@ -32,12 +27,14 @@ public class ThreeSum {
                     l++;
                 }else{
                     result.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                    do{r--;}while(l<r&&nums[r]==nums[r-1]);
-                    do{l++;}while(l<r&&nums[l]==nums[l+1]);
+                    while(l<r&&nums[r]==nums[r-1]){r--;}
+                    while(l<r&&nums[l]==nums[l+1]){l++;}
+                    r--;l++;
                 }
             }
         }
         return result;
     }
-
 }
+//runtime:32 ms
+//memory:48.1 MB
