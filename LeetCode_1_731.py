4911054343de928a36c0cf25c432a4a513e437ce
Maxlychee <30388142+Maxlychee@users.noreply.github.com>
2019-10-20 23:05:04 +0800

    delete file
    NA

--- LeetCode_1_731.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        lens = len(nums)
-        for i in range(lens):
-            if target - nums[i] in nums[i+1:]:
-                return [i, nums.index(target - nums[i], i+1)]
