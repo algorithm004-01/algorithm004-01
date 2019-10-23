e91c687a09e75b8b6a6fa0457240776d880cf172
cool <cool_kj@126.com>
2019-10-24 00:27:51 +0800

    226. 翻转二叉树 递归解法

---
+++ Week 02/id_251/LeetCode_226_251.py
@@ -0,0 +1,44 @@
+# 翻转一棵二叉树。
+#
+# 示例： 
+#
+# 输入： 
+#
+#      4
+#   /   \
+#  2     7
+# / \   / \
+# 1   3 6   9
+#
+# 输出： 
+#
+#      4
+#   /   \
+#  7     2
+# / \   / \
+# 9   6 3   1
+#
+# 备注: 
+# 这个问题是受到 Max Howell 的 原问题 启发的 ：
+#
+# 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。 
+# Related Topics 树
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None: return None
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
