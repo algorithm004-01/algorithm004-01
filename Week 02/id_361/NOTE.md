0291fce5dafbadb298349d533672e3a3dab6c6a4
Zachy <duyuan30@126.com>
2019-10-27 23:53:25 +0800

    week 2

--- Week 02/id_361/NOTE.md
+++ Week 02/id_361/NOTE.md
@@ -1,4 +1,41 @@
-# NOTE
-
+### 第二周总结（持续更新中...）
   
+
+#### 1.树
+
+树 是没有环的图
+- 生活的树：
+-- alpha go  
+- decision 
+- 人生状态 、游戏
+
+#### 2.树的面试题解法一般都是递归，为什么？
+
+[递归的概念-知乎](https://www.zhihu.com/question/20507130)
+
+![Image text](https://pic3.zhimg.com/80/1f818b686dc5482cbb8343d8caf65dac_hd.jpg)
+
+- 注意：递归不一定会影响效率
+
+#### 3.树的常见操作
+- 查询
+log2n 类似于二分查找
+- 删除
+删除 在叶子上直接删除 在其他位置的必须找到替换节点
+
+- 遍历
+
+preorder traversal 先序遍历
+根左右
+
+inorder traversal 中序遍历
+左根右
+
+postorder traversal 后续遍历
+左右根
+
+
+
+
+
 
--- Week 02/id_361/leetCode_1_361.js
+++ Week 02/id_361/leetCode_1_361.js
@@ -2,6 +2,9 @@
 
 /**
  * javascript
+ * 1.暴力法
+ * 时间复杂度：O(n^2)
+ * 空间复杂度：O(1)
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
@@ -17,4 +20,48 @@ var twoSum = function(nums, target) {
         }
     }
     return arr
+};
+
+/**
+ * javascript
+ * 2.两遍哈希表
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let  map = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], i);
+    }
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement) && map.get(complement) != i) {
+            return [ i, map.get(complement) ];
+        }
+    }
+    throw new Error("No two sum solution");
+};
+
+/**
+ * javascript
+ * 2.两遍哈希表
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    throw new Error("No two sum solution");
 };
