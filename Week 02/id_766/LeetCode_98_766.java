a0ff6cc021e8340f29e5760e3cd2d7c4c8dbd9f2
yongliRUC <yongliyy2017@ruc.edu.cn>
2019-10-28 03:51:20 +0800

    Validate Binary Search Tree

---
+++ Week 02/id_766/LeetCode_98_766.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=java
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode pre = null;
+
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        if (!isValidBST(root.left)) return false;
+        if (pre != null && pre.val >= root.val) return false;
+        pre = root;
+        return isValidBST(root.right);
+
+    }
+}
+// @lc code=end
+
