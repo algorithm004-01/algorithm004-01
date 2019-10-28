db8314936ef5bfc6b8d22e25deaf95cbadc225c5
karringqing <823307513@qq.com>
2019-10-28 19:25:34 +0800

    LeetCode_102_416

---
+++ Week 03/id_416/LeetCode_22_416.java
@@ -0,0 +1,39 @@
+/**
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ *
+ * 例如，给出 n = 3，生成结果为：
+ *
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/generate-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode_22_416{
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        _generate("",0,0,2 * n,list);
+        return list;
+    }
+    public void _generate(String item,int left,int right,int max,List<String> list){
+        if(left == max/2 && right == max/2 ) {//terminator
+            list.add(item);
+            return;
+        }
+        if(left < max/2) {//左括号要小于n，那么我们还可以用一个括号
+            //drill down
+            _generate(item+"(",left+1,right,max,list);
+        }
+        if(right < left) {//右括号要小于左括号
+            //drill down
+            _generate(item+")",left,right + 1,max,list);
+        }
+        //restore states
+    }
+}
