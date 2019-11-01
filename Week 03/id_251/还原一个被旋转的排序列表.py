ec545928255bf9aff5cd204a799eedb50186b1a3
cool <cool_kj@126.com>
2019-11-02 00:46:00 +0800

    还原一个被旋转的排序列表

--- Week 03/id_251/还原一个被旋转的排序列表.py
+++ Week 03/id_251/还原一个被旋转的排序列表.py
@@ -1,13 +1,20 @@
+"""
+约束条件：列表中没有重复元素
+"""
+
+
 def revert(array):
     left, right = 0, len(array) - 1
     while left < right:
         mid = left + (right - left) // 2
-        if array[left] < array[mid]:
+        if array[left] < array[mid] < array[right]:
+            return array
+        elif array[left] < array[mid]:
             left = mid
         else:
             right = mid
 
-    return right
+    return array[left + 1:] + array[: right + 1]
 
 
 if __name__ == '__main__':
@@ -17,3 +24,4 @@ if __name__ == '__main__':
     print(revert([4, 5, 6, 7, -3, -2, 0, 1, 2]))
     print(revert([]))
     print(revert([1]))
+    print(revert([1, 2, 3]))
