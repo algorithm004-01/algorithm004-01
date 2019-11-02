49e95bfe6cb4afa932ef416bbd71ce55120698d8
suanzhai <343628123@qq.com>
2019-11-02 21:49:29 +0800

    Update LeetCode_45_491.java
    拷贝错了，把实验LeetCode题解的代码提交了，没有提交我自己的代码。

--- Week 03/id_491/LeetCode_45_491.java
+++ Week 03/id_491/LeetCode_45_491.java
@@ -1,17 +1,31 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length == 1) return 0;
-        int reach = 0;
-        int nextreach = nums[0];
-        int step = 0;
-        for(int i = 0; i<nums.length; i++){
-            nextreach = Math.max(i+nums[i],nextreach);
-            if(nextreach >= nums.length-1) return (step+1);
-            if(i == reach){
-                step++;
-                reach = nextreach;
+        if(nums.length == 1){
+            return 0;
+        } 
+
+        int steps = 0;
+        List<Integer> stepsList = new ArrayList<>();
+        stepsList.add(0);
+        int maxIndex = 0;
+        while(steps < nums.length - 1 && stepsList.size() > 0) {
+            steps++;
+            List<Integer> tempList = new ArrayList<>();
+            for(int j = 0; j < stepsList.size(); j++) {
+                int index = stepsList.get(j);
+                for(int i = nums[index]; i + index > maxIndex; i--) {
+                    if(index + i >= nums.length - 1){
+                        return steps;
+                    } 
+
+                    tempList.add(index + i);
+                }
+                maxIndex = nums[index] + index;
             }
+
+            stepsList = tempList;
         }
-        return step;
+
+        return -1;
     }
 }
