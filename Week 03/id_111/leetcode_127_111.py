af750d4c365d02560272c893c50bcc7370854f13
Fangbaso4 <44719631+Fangbaso4@users.noreply.github.com>
2019-11-03 21:49:45 +0800

    Update leetcode_127_111.py

--- Week 03/id_111/leetcode_127_111.py
+++ Week 03/id_111/leetcode_127_111.py
@@ -11,3 +11,68 @@
 字典中不存在重复的单词。
 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
 """
+#广度优先 BFS 代码模板：
+def BFS(graph, start, end):
+    queue = []
+    queue.append([start])
+    visited.add(start)
+
+    while queue:
+        node = queue.pop()
+        visited.add(node)
+
+        process(node)
+        nodes = generate_related_nodes(node)
+        queue.push(nodes)
+#代码解决：（实在写不出来抄了一遍）
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        if endWord not in wordList:
+            return 0
+        # 前提：单词字典中所有单词长度一致
+        L = len(beginWord)
+
+        # Dictionary to hold combination of words that can be formed,
+        # from any given word. By changing one letter at a time.
+        all_combo_dict = defaultdict(list)
+        for word in wordList:
+            for i in range(L):
+                # Key is the generic word
+                # Value is a list of words which have the same intermediate generic word.
+                all_combo_dict[word[:i] + "*" + word[i+1:]].append(word)
+
+
+        # 建立 BFS 队列
+        queue = collections.deque([(beginWord, 1)])
+        # 建立是否访问 避免重复访问
+        visited = {beginWord: True}
+        while queue:
+            current_word, level = queue.popleft()      
+            for i in range(L):
+                # Intermediate words for current word
+                intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+
+                # Next states are all the words which share the same intermediate state.
+                for word in all_combo_dict[intermediate_word]:
+                    # If at any point if we find what we are looking for
+                    # i.e. the end word - we can return with the answer.
+                    if word == endWord:
+                        return level + 1
+                    # Otherwise, add it to the BFS Queue. Also mark it visited
+                    if word not in visited:
+                        visited[word] = True
+                        queue.append((word, level + 1))
+                all_combo_dict[intermediate_word] = []
+        return 0
+
+        # queue = set()
+        # visited = set()
+        # visitied.add(beginWord)
+
+        # word_len = len(beginWord)
+        # res = 1
+        # #BFS
+        # while queue:
+        #     node = queue
+
