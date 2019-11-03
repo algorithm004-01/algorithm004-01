0a87cb072b73ea30c391125259bf3a344f2139a2
caoning <caoning07@meituan.com>
2019-11-04 00:54:20 +0800

    第三周作业

--- Week 03/id_311/LeetCode_126_Solution.java
+++ Week 03/id_311/LeetCode_126_Solution.java
@@ -4,11 +4,9 @@ class Solution {
         if (!wordList.contains(endWord)) {
             return ans;
         }
-        // 利用 BFS 得到所有的邻居节点
         HashMap<String, ArrayList<String>> map = new HashMap<>();
         bfs(beginWord, endWord, wordList, map);
         ArrayList<String> temp = new ArrayList<String>();
-        // temp 用来保存当前的路径
         temp.add(beginWord);
         findLaddersHelper(beginWord, endWord, map, temp, ans);
         return ans;
@@ -20,7 +18,6 @@ class Solution {
             ans.add(new ArrayList<String>(temp));
             return;
         }
-        // 得到所有的下一个的节点
         ArrayList<String> neighbors = map.getOrDefault(beginWord, new ArrayList<String>());
         for (String neighbor : neighbors) {
             temp.add(neighbor);
@@ -29,7 +26,6 @@ class Solution {
         }
     }
 
-    //利用递归实现了双向搜索
     private void bfs(String beginWord, String endWord, List<String> wordList, HashMap<String, ArrayList<String>> map) {
         Set<String> set1 = new HashSet<String>();
         set1.add(beginWord);
@@ -39,58 +35,38 @@ class Solution {
         bfsHelper(set1, set2, wordSet, true, map);
     }
 
-    // direction 为 true 代表向下扩展，false 代表向上扩展
     private boolean bfsHelper(Set<String> set1, Set<String> set2, Set<String> wordSet, boolean direction,
                               HashMap<String, ArrayList<String>> map) {
-        //set1 为空了，就直接结束
-        //比如下边的例子就会造成 set1 为空
-    /*	"hot"
-		"dog"
-		["hot","dog"]*/
         if (set1.isEmpty()) {
             return false;
         }
-        // set1 的数量多，就反向扩展
         if (set1.size() > set2.size()) {
             return bfsHelper(set2, set1, wordSet, !direction, map);
         }
-        // 将已经访问过单词删除
         wordSet.removeAll(set1);
         wordSet.removeAll(set2);
 
         boolean done = false;
 
-        // 保存新扩展得到的节点
         Set<String> set = new HashSet<String>();
 
         for (String str : set1) {
-            //遍历每一位
             for (int i = 0; i < str.length(); i++) {
                 char[] chars = str.toCharArray();
-
-                // 尝试所有字母
                 for (char ch = 'a'; ch <= 'z'; ch++) {
                     if (chars[i] == ch) {
                         continue;
                     }
                     chars[i] = ch;
-
                     String word = new String(chars);
-
-                    // 根据方向得到 map 的 key 和 val
                     String key = direction ? str : word;
                     String val = direction ? word : str;
-
                     ArrayList<String> list = map.containsKey(key) ? map.get(key) : new ArrayList<String>();
-
-                    //如果相遇了就保存结果
                     if (set2.contains(word)) {
                         done = true;
                         list.add(val);
                         map.put(key, list);
                     }
-
-                    //如果还没有相遇，并且新的单词在 word 中，那么就加到 set 中
                     if (!done && wordSet.contains(word)) {
                         set.add(word);
                         list.add(val);
@@ -99,8 +75,6 @@ class Solution {
                 }
             }
         }
-
-        //一般情况下新扩展的元素会多一些，所以我们下次反方向扩展  set2
         return done || bfsHelper(set2, set, wordSet, !direction, map);
 
     }
