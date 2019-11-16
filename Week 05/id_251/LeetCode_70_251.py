9b0d35f5705e6f19a3498ad1c0cc32efbe4d40f3
cool <cool_kj@126.com>
2019-11-16 17:08:48 +0800

    70. 爬楼梯 DP list方法 减少一些冗余代码

--- Week 05/id_251/LeetCode_70_251.py
+++ Week 05/id_251/LeetCode_70_251.py
@@ -60,8 +60,6 @@ class Solution(object):
     def iteration_DP(self, n):
         if n == 1:
             return 1
-        if n == 2:
-            return 2
         dp = [1, 2] + [0] * (n - 2)
         for i in range(2, n):
             dp[i] = dp[i - 1] + dp[i - 2]
