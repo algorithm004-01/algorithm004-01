058446fe5173be686266d74c43c4e4727b86b8b1
cool <cool_kj@126.com>
2019-11-16 12:25:38 +0800

    64. 最小路径和 修改错误漏掉一行

--- Week 05/id_251/LeetCode_64_251.py
+++ Week 05/id_251/LeetCode_64_251.py
@@ -54,6 +54,7 @@ class Solution2(object):
         """
         m, n = len(grid), len(grid[0])
         dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = grid[0][0]
         for i in range(1, n):
             dp[0][i] = dp[0][i - 1] + grid[0][i]
         for i in range(1, m):
