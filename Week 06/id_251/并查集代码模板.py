7c914396aa6dd74ceb05d34d8d91e9113cadee51
cool <cool_kj@126.com>
2019-11-24 10:59:40 +0800

    并查集代码模板 优化

--- Week 06/id_251/并查集代码模板.py
+++ Week 06/id_251/并查集代码模板.py
@@ -3,14 +3,28 @@ class UnionFind:
         self.u = list(range(n))
 
     def union(self, i, j):
-        p1 = self.parent(i)
-        p2 = self.parent(j)
+        p1 = self.find(i)
+        p2 = self.find(j)
         self.u[p1] = p2
 
-    def parent(self, i):
+    def find(self, i):
         root = i
         while self.u[root] != root:
             root = self.u[root]
         while self.u[i] != i:
-            i, self.u[i] = self.u[i], root
+            self.u[i], i = root, self.u[i]
         return root
+
+
+if __name__ == '__main__':
+    uf = UnionFind(6)
+    uf.union(3, 0)
+    print(uf.u)
+    uf.union(4, 2)
+    print(uf.u)
+    uf.union(2, 1)
+    print(uf.u)
+    uf.union(4, 0)
+    print(uf.u)
+    uf.union(4, 0)
+    print(uf.u)
