f82dcc966acb14c670797a3fa0350cdb0498cee4
hadronw <hadronw@qq.com>
2019-11-30 10:04:31 +0800

    086-Week 07

---
+++ Week 07/id_086/LeetCode_1122_086.java
@@ -0,0 +1,16 @@
+/**
+ *  1122.有效的字母异位词
+ */
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars,tChars);
+    }
+}
