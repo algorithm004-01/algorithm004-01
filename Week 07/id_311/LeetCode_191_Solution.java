f99c76943d0e0c6d8ea21f6d2239f92584c1f9b7
caoning <caoning07@meituan.com>
2019-12-01 17:44:00 +0800

    第七周作业

--- Week 07/id_311/LeetCode_191_Solution.java
+++ Week 07/id_311/LeetCode_191_Solution.java
@@ -1,5 +1,11 @@
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        return n > 0 && (n & (n -1)) == 0;
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while(n != 0){
+            sum++;
+            n &= (n -1);
+        }
+        return sum;
     }
 }
