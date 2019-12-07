43fc0766425acc18313fe047d8ac98ecb3ce3413
caoning <919542694@qq.com>
2019-12-07 22:58:33 +0800

    第七周作业

---
+++ Week 07/id_311/LeetCode_300_Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+}
