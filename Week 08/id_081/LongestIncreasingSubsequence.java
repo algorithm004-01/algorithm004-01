700e0307f137bfd803b1f91408e2754f51b50b0d
JianRan <summerspsp@gmail.com>
2019-12-08 23:45:26 +0800

    Week 08

---
+++ Week 08/id_081/LongestIncreasingSubsequence.java
@@ -0,0 +1,15 @@
+class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        int res = 0;
+        Arrays.fill(dp, 1);
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
