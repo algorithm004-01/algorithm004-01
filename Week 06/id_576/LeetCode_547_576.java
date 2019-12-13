08b6ed22be78f3a6e111eafff07b7957343afe9f
Nicole Lei <40916016+Leith14@users.noreply.github.com>
2019-12-13 15:39:29 +0800

    Update LeetCode_547_576.java

---
+++ Week 06/id_576/LeetCode_547_576.java
@@ -0,0 +1,53 @@
+package id_576;
+
+public class LeetCode_547_576 {
+
+    class UnionFind {
+        private int count = 0;
+        private int[] parent, rank;
+        public UnionFind(int n) {
+            count = n;
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int p) {
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) return;
+            if (rank[rootQ] > rank[rootP]) {
+                parent[rootP] = rootQ;
+            } else {
+                parent[rootQ] = rootP;
+                if (rank[rootP] == rank[rootQ]) {
+                    rank[rootP]++;
+                }
+            }
+            count--;
+        }
+        public int count() {
+            return count;
+        }
+
+    }
+    public int findCircleNum(int[][] M) {
+        int n = M.length;
+        UnionFind uf = new UnionFind(n);
+        for (int i = 0; i < n - 1 ; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (M[i][j] == 1) uf.union(i, j);
+            }
+        }
+        return uf.count();
+    }
+}
