a55d6961bbca6652b7402222cdc351c3fc288722
Nicole Lei <40916016+Leith14@users.noreply.github.com>
2019-12-16 15:03:00 +0800

    Create LeetCode_49_576.java

---
+++ Week 02/id_576/LeetCode_49_576.java
@@ -0,0 +1,41 @@
+package id_576;
+
+import java.util.*;
+
+public class LeetCode_49_576 {
+
+    // Approach 1: sort, O(k * n * logN)
+    public List<List<String>> groupAnagrams1(String[] strs) {
+        if (strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> ans = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+            ans.get(key).add(s);
+        }
+        return new ArrayList<>(ans.values());
+    }
+
+    // Approach 2: counter, O(k * n * logN)
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        if (strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> ans = new HashMap<>();
+        int[] count = new int[26];
+        for (String s: strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+            ans.get(key).add(s);
+        }
+        return new ArrayList<>(ans.values());
+    }
+}
